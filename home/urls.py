ff28927436147eacd468acdab577929663dad7f2
ClearSky <ggogono2@gmail.com>
2022-07-15 11:26:08 +0900

    url

--- home/urls.py
+++ home/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path('', views.home),
     path('home', views.home, name='home'),
     path('user', views.tbd, name='user'),
-    path('signin', views.tbd),
-    path('signup', views.tbd),
-    path('guide', views.tbd),
+    path('signin', views.tbd, name='signin'),
+    path('signup', views.tbd, name='signup'),
+    path('guide', views.tbd, name='guide'),
     path('timer', views.tbd),
 
 ]
--- home/views.py
+++ home/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 def home(request):
     if request.user.is_authenticated:
         return redirect('user')
-    return render(request, 'base.html')
+    return render(request, 'home.html')
 
 def tbd(request):
     return HttpResponse("to be done...")
